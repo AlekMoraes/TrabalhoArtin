37cf5c4c54ca062c09bcebc65f86aa7471d2689c
Alex Moraes <alex.bfm@hotmail.com>
2021-06-15 19:13:54 -0300

    Finalização prova

---
+++ controllers/public-produtos.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const Produto = require("../models/produtos")
+
+const route = express.Router()
+
+route.get("/", async (req, res) => {
+    var lista = await Produto.find()
+    return res.send(lista)
+})
+
+module.exports = app => app.use("/lista/produtos", route)
